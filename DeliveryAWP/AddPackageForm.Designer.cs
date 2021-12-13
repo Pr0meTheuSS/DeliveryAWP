fe294fd7fd4220964b20a9765f32d6ece24a53b5
Yriy Olimpiev <y.olimpiev@g.nsu.ru>
2021-12-13 15:17:08 +0700

    Добавил форму для создания карточки посылки

---
+++ DeliveryAWP/AddPackageForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace DeliveryAWP
+{
+    partial class AddPackageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.ReciverTextBox = new System.Windows.Forms.TextBox();
+            this.CreatedDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SenderTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(646, 343);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(124, 23);
+            this.CancelButton.TabIndex = 19;
+            this.CancelButton.Text = "Отмена";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // ReciverTextBox
+            // 
+            this.ReciverTextBox.Location = new System.Drawing.Point(33, 251);
+            this.ReciverTextBox.Name = "ReciverTextBox";
+            this.ReciverTextBox.Size = new System.Drawing.Size(737, 22);
+            this.ReciverTextBox.TabIndex = 12;
+            this.ReciverTextBox.TextChanged += new System.EventHandler(this.ReciverTextBox_TextChanged);
+            // 
+            // CreatedDateTimePicker
+            // 
+            this.CreatedDateTimePicker.Location = new System.Drawing.Point(33, 326);
+            this.CreatedDateTimePicker.Name = "CreatedDateTimePicker";
+            this.CreatedDateTimePicker.Size = new System.Drawing.Size(266, 22);
+            this.CreatedDateTimePicker.TabIndex = 13;
+            this.CreatedDateTimePicker.ValueChanged += new System.EventHandler(this.CreatedDateTimePicker_ValueChanged);
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(506, 343);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(124, 23);
+            this.CreateButton.TabIndex = 18;
+            this.CreateButton.Text = "Создать";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Данные отправителя";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Данные получателя";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Дата создания карточки посылки";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // SenderTextBox
+            // 
+            this.SenderTextBox.Location = new System.Drawing.Point(33, 179);
+            this.SenderTextBox.Name = "SenderTextBox";
+            this.SenderTextBox.Size = new System.Drawing.Size(737, 22);
+            this.SenderTextBox.TabIndex = 11;
+            this.SenderTextBox.TextChanged += new System.EventHandler(this.SenderTextBox_TextChanged);
+            // 
+            // AddPackageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CreatedDateTimePicker);
+            this.Controls.Add(this.ReciverTextBox);
+            this.Controls.Add(this.SenderTextBox);
+            this.Name = "AddPackageForm";
+            this.Text = "AddPackageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox ReciverTextBox;
+        private System.Windows.Forms.DateTimePicker CreatedDateTimePicker;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox SenderTextBox;
+    }
+}
---
+++ DeliveryAWP/AddPackageForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeliveryAWP
+{
+    public partial class AddPackageForm : Form
+    {
+        public AddPackageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SenderTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreatedDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReciverTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
